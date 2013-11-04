8d89f52a3101529cee8f7fa945527ac20d5d8bbb
Christian Schulte <c.schulte@geomobile.de>
2013-11-04 12:54:39 +0100

    scale image

--- Nanogramme/src/picture/PictureService.java
+++ Nanogramme/src/picture/PictureService.java
@@ -1,56 +1,86 @@
 package picture;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
+/**
+ * Diese Klasse ist für das Erstellen eines Rätsels aus einem Bild zuständig.
+ * 
+ * @author cschulte
+ * 
+ */
 public class PictureService {
 
-   public void insertNode(Color color, Node root) {
-      int red = color.getRed();
-      int green = color.getGreen();
-      int blue = color.getBlue();
-      
-      String binary11 = Integer.toBinaryString(red);
-      String binary12 = Integer.toBinaryString(green);
-      String binary13 = Integer.toBinaryString(blue);
-      
-      if (binary11.length() < 8) {
-         for (int i = binary11.length(); i < 8; i++) {
-            binary11 = "0" + binary11;
-         }
-      }
-      System.out.println(binary11);
-      
-      if (binary12.length() < 8) {
-         for (int i = binary12.length(); i < 8; i++) {
-            binary12 = "0" + binary12;
-         }
-      }
-      
-      System.out.println(binary13);
-      
-      if (binary13.length() < 8) {
-         for (int i = binary13.length(); i < 8; i++) {
-            binary13 = "0" + binary13;
-         }
-      }
-      System.out.println(binary13);
-      
-      
-      Node node = root;
-      for (int i = 0; i < 8; i++) {
-         String indexString = "" + (binary11.charAt(i) + "" + binary12.charAt(i) + "" + binary13.charAt(i));
-         int index = Integer.parseInt(indexString, 2);
-         System.out.println(index);
-         if (node.getNode(index) == null) {
-            node.setNode(index);
-         }
-         node = node.getNode(index);
-      }
-      node.setRed(red);
-      node.setGreen(green);
-      node.setBlue(blue);
-      node.setReferences(node.getReferences() + 1);
+	/**
+	 * Fügt einen Pixel (Farbe) dem root Element hinzu. Dies muss für alle Pixel
+	 * des Bildes gemacht werden.
+	 * 
+	 * @param color
+	 *            Die Farbe des Pixels.
+	 * @param root
+	 *            Das root Element, in das alle Pixel hinzugefügt werden.
+	 */
+	public void insertNode(Color color, Node root) {
+		int red = color.getRed();
+		int green = color.getGreen();
+		int blue = color.getBlue();
 
-   }
+		String binary11 = Integer.toBinaryString(red);
+		String binary12 = Integer.toBinaryString(green);
+		String binary13 = Integer.toBinaryString(blue);
+
+		if (binary11.length() < 8) {
+			for (int i = binary11.length(); i < 8; i++) {
+				binary11 = "0" + binary11;
+			}
+		}
+		// System.out.println(binary11);
+
+		if (binary12.length() < 8) {
+			for (int i = binary12.length(); i < 8; i++) {
+				binary12 = "0" + binary12;
+			}
+		}
+
+		// System.out.println(binary13);
+
+		if (binary13.length() < 8) {
+			for (int i = binary13.length(); i < 8; i++) {
+				binary13 = "0" + binary13;
+			}
+		}
+		// System.out.println(binary13);
+
+		Node node = root;
+		for (int i = 0; i < 8; i++) {
+			String indexString = ""
+					+ (binary11.charAt(i) + "" + binary12.charAt(i) + "" + binary13
+							.charAt(i));
+			int index = Integer.parseInt(indexString, 2);
+			// System.out.println(index);
+			if (node.getNode(index) == null) {
+				node.setNode(index);
+			}
+			node = node.getNode(index);
+		}
+		node.setRed(red);
+		node.setGreen(green);
+		node.setBlue(blue);
+		node.setReferences(node.getReferences() + 1);
+
+	}
+	 /**
+	  * Skalliert das Bild.
+	 * @param originalImage 
+	  */
+	public BufferedImage scalePicture(int width, int height, Image originalImage, int type) {
+		BufferedImage bi = new BufferedImage(width, height, type);
+		Graphics2D g = bi.createGraphics();
+		g.drawImage(originalImage, 0, 0, width, height, null);
+		g.dispose();
+		return bi;
+	}
 
 }
--- Nanogramme/src/picture/PictureTestClass.java
+++ Nanogramme/src/picture/PictureTestClass.java
@@ -1,5 +1,12 @@
 package picture;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class PictureTestClass {
    
    public static void main(String[] args) {
@@ -8,112 +15,28 @@ public class PictureTestClass {
       int color2 = 30;
       int color3 = 20;
       
-      String binary1 = Integer.toBinaryString(color1);
-      String binary2 = Integer.toBinaryString(color2);
-      String binary3 = Integer.toBinaryString(color3);
-      
       int color11 = 30;
       int color12 = 130;
       int color13 = 224;
+      PictureService ps = new PictureService();
       
-      String binary11 = Integer.toBinaryString(color11);
-      String binary12 = Integer.toBinaryString(color12);
-      String binary13 = Integer.toBinaryString(color13);
+      try {
+		BufferedImage oi = ImageIO.read(new File("test.jpg"));
+		int type = oi.getType() == 0? BufferedImage.TYPE_INT_ARGB : oi.getType();
+		BufferedImage resizedImage = ps.scalePicture(150, 150, oi, type);
+		ImageIO.write(resizedImage, "jpg", new File("test2.jpg"));
+		
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
       
-      if (binary1.length() < 8) {
-         for (int i = binary1.length(); i < 8; i++) {
-            binary1 = "0" + binary1;
-         }
-      }
-      System.out.println(binary1);
-      
-      if (binary2.length() < 8) {
-         for (int i = binary2.length(); i < 8; i++) {
-            binary2 = "0" + binary2;
-         }
-      }
-      
-      System.out.println(binary2);
-      
-      if (binary3.length() < 8) {
-         for (int i = binary3.length(); i < 8; i++) {
-            binary3 = "0" + binary3;
-         }
-      }
-      System.out.println(binary3);
-      
-      if (binary11.length() < 8) {
-         for (int i = binary11.length(); i < 8; i++) {
-            binary11 = "0" + binary11;
-         }
-      }
-      System.out.println(binary11);
-      
-      if (binary12.length() < 8) {
-         for (int i = binary12.length(); i < 8; i++) {
-            binary12 = "0" + binary12;
-         }
-      }
-      
-      System.out.println(binary12);
-      
-      if (binary13.length() < 8) {
-         for (int i = binary13.length(); i < 8; i++) {
-            binary13 = "0" + binary13;
-         }
-      }
-      System.out.println(binary13);
-      
+      Color color = new Color(color1, color2, color3);
+      Color colorZwei = new Color(color11, color12, color13);
       Node root = new Node();
-      Node node = root;
-      for (int i = 0; i < 8; i++) {
-         String indexString = "" + (binary1.charAt(i) + "" + binary2.charAt(i)+ "" + binary3.charAt(i));
-         int index = Integer.parseInt(indexString, 2);
-         System.out.println(index);
-         if (node.getNode(index) == null) {
-            node.setNode(index);
-         }
-         node = node.getNode(index);
-      }
-      node.setRed(color1);
-      node.setGreen(color2);
-      node.setBlue(color3);
-      node.setReferences(1);
-      
-      System.out.println(root);
-      
-      node = root;
-      for (int i = 0; i < 8; i++) {
-         String indexString = "" + (binary11.charAt(i) + "" + binary12.charAt(i)+ "" + binary13.charAt(i));
-         int index = Integer.parseInt(indexString, 2);
-         System.out.println(index);
-         if (node.getNode(index) == null) {
-            node.setNode(index);
-         }
-         node = node.getNode(index);
-      }
-      node.setRed(color11);
-      node.setGreen(color12);
-      node.setBlue(color13);
-      node.setReferences(node.getReferences() +1);
-      
-      System.out.println(root);
-      
-      node = root;
-      for (int i = 0; i < 8; i++) {
-         String indexString = "" + (binary11.charAt(i) + "" + binary12.charAt(i)+ "" + binary13.charAt(i));
-         int index = Integer.parseInt(indexString, 2);
-         System.out.println(index);
-         if (node.getNode(index) == null) {
-            node.setNode(index);
-         }
-         node = node.getNode(index);
-      }
-      node.setRed(color11);
-      node.setGreen(color12);
-      node.setBlue(color13);
-      node.setReferences(node.getReferences() +1);
-      
+     
+      ps.insertNode(color, root);
+      ps.insertNode(colorZwei, root);
+      ps.insertNode(colorZwei, root);
       System.out.println(root);
       
    }
