a27f20b620664c19dc9ccceb1318f26876dda109
Christian Schulte genannt Trux <csgt@m4guides-MacBook-Pro.local>
2013-11-07 22:47:45 +0100

    colors

--- Nanogramme/src/picture/PictureService.java
+++ Nanogramme/src/picture/PictureService.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
+
+import javax.imageio.ImageIO;
 
 /**
  * Diese Klasse ist für das Erstellen eines Rätsels aus einem Bild zuständig.
@@ -14,226 +18,243 @@ import java.util.LinkedList;
  */
 public class PictureService {
 
-	/**
-	 * Fügt einen Pixel (Farbe) dem root Element hinzu. Dies muss für alle Pixel
-	 * des Bildes gemacht werden.
-	 * 
-	 * @param color
-	 *            Die Farbe des Pixels.
-	 * @param root
-	 *            Das root Element, in das alle Pixel hinzugefügt werden.
-	 */
-	public void insertNode(Color color, Node root) {
-		int red = color.getRed();
-		int green = color.getGreen();
-		int blue = color.getBlue();
+   /**
+    * Fügt einen Pixel (Farbe) dem root Element hinzu. Dies muss für alle Pixel
+    * des Bildes gemacht werden.
+    * 
+    * @param color
+    *           Die Farbe des Pixels.
+    * @param root
+    *           Das root Element, in das alle Pixel hinzugefügt werden.
+    */
+   public void insertNode(Color color, Node root) {
+      int red = color.getRed();
+      int green = color.getGreen();
+      int blue = color.getBlue();
 
-		String binary11 = Integer.toBinaryString(red);
-		String binary12 = Integer.toBinaryString(green);
-		String binary13 = Integer.toBinaryString(blue);
+      String binary11 = Integer.toBinaryString(red);
+      String binary12 = Integer.toBinaryString(green);
+      String binary13 = Integer.toBinaryString(blue);
 
-		if (binary11.length() < 8) {
-			for (int i = binary11.length(); i < 8; i++) {
-				binary11 = "0" + binary11;
-			}
-		}
-		// System.out.println(binary11);
+      if (binary11.length() < 8) {
+         for (int i = binary11.length(); i < 8; i++) {
+            binary11 = "0" + binary11;
+         }
+      }
+      // System.out.println(binary11);
 
-		if (binary12.length() < 8) {
-			for (int i = binary12.length(); i < 8; i++) {
-				binary12 = "0" + binary12;
-			}
-		}
+      if (binary12.length() < 8) {
+         for (int i = binary12.length(); i < 8; i++) {
+            binary12 = "0" + binary12;
+         }
+      }
 
-		// System.out.println(binary13);
+      // System.out.println(binary13);
 
-		if (binary13.length() < 8) {
-			for (int i = binary13.length(); i < 8; i++) {
-				binary13 = "0" + binary13;
-			}
-		}
-		// System.out.println(binary13);
+      if (binary13.length() < 8) {
+         for (int i = binary13.length(); i < 8; i++) {
+            binary13 = "0" + binary13;
+         }
+      }
+      // System.out.println(binary13);
 
-		Node node = root;
-		// Node lastNode = root;
-		for (int i = 0; i < 8; i++) {
-			String indexString = ""
-					+ (binary11.charAt(i) + "" + binary12.charAt(i) + "" + binary13
-							.charAt(i));
-			int index = Integer.parseInt(indexString, 2);
-			// System.out.println(index);
-			if (node.getNode(index) == null) {
-				node.setNode(index, new Node());
-			}
-			// lastNode = node;
-			node = node.getNode(index);
-		}
-		// lastNode.setReferencesOfChilds();
-		node.setRed(node.getRed() + red);
-		node.setGreen(node.getGreen() + green);
-		node.setBlue(node.getBlue() + blue);
-		node.setReferences(node.getReferences() + 1);
+      Node node = root;
+      // Node lastNode = root;
+      for (int i = 0; i < 8; i++) {
+         String indexString = "" + (binary11.charAt(i) + "" + binary12.charAt(i) + "" + binary13.charAt(i));
+         int index = Integer.parseInt(indexString, 2);
+         // System.out.println(index);
+         if (node.getNode(index) == null) {
+            node.setNode(index, new Node());
+         }
+         // lastNode = node;
+         node = node.getNode(index);
+      }
+      // lastNode.setReferencesOfChilds();
+      node.setRed(node.getRed() + red);
+      node.setGreen(node.getGreen() + green);
+      node.setBlue(node.getBlue() + blue);
+      node.setReferences(node.getReferences() + 1);
 
-	}
+   }
 
-	/**
-	 * Skalliert das Bild.
-	 * 
-	 * @param originalImage
-	 */
-	public BufferedImage scalePicture(int width, int height,
-			Image originalImage, int type) {
-		BufferedImage bi = new BufferedImage(width, height, type);
-		Graphics2D g = bi.createGraphics();
-		g.drawImage(originalImage, 0, 0, width, height, null);
-		g.dispose();
-		return bi;
-	}
+   /**
+    * Skalliert das Bild.
+    * 
+    * @param originalImage
+    */
+   public BufferedImage scalePicture(int width, int height, Image originalImage, int type) {
+      BufferedImage bi = new BufferedImage(width, height, type);
+      Graphics2D g = bi.createGraphics();
+      g.drawImage(originalImage, 0, 0, width, height, null);
+      g.dispose();
+      return bi;
+   }
 
-	// public void setReferenceChilds(Node root) {
-	// boolean mustDo = false;
-	// for (int i = 0; i < root.getNodes().length; i++) {
-	// if (root.getNode(i) != null) {
-	// if (root.getNode(i).getReferences() == 0) {
-	// setReferenceChilds(root.getNode(i));
-	// } else {
-	// if (root.getReferencesOfChilds() == null) {
-	// mustDo = true;
-	// }
-	// }
-	// }
-	// }
-	// if (mustDo) {
-	// int childsReferences = 0;
-	// for (int i = 0; i < root.getNodes().length; i++) {
-	// if (root.getNode(i) != null) {
-	// childsReferences += root.getNode(i).getReferences();
-	// }
-	// }
-	// root.setReferencesOfChilds(childsReferences);
-	// }
-	// }
+   // public void setReferenceChilds(Node root) {
+   // boolean mustDo = false;
+   // for (int i = 0; i < root.getNodes().length; i++) {
+   // if (root.getNode(i) != null) {
+   // if (root.getNode(i).getReferences() == 0) {
+   // setReferenceChilds(root.getNode(i));
+   // } else {
+   // if (root.getReferencesOfChilds() == null) {
+   // mustDo = true;
+   // }
+   // }
+   // }
+   // }
+   // if (mustDo) {
+   // int childsReferences = 0;
+   // for (int i = 0; i < root.getNodes().length; i++) {
+   // if (root.getNode(i) != null) {
+   // childsReferences += root.getNode(i).getReferences();
+   // }
+   // }
+   // root.setReferencesOfChilds(childsReferences);
+   // }
+   // }
 
-	public void reduceColors(Node root) {
-		root.setReferences(root.getReferencesOfChilds());
-		root.setReferencesOfChilds(0);
-		root.children = 0;
-		Node[] nodes = root.getNodes();
-		for (int i = 0; i < nodes.length; i++) {
-			if (root.getNode(i) != null) {
-				reduceColors(root.getNode(i));
-				root.setNode(i, null);
-			}
-		}
-	}
+   public void reduceColors(Node root) {
+      root.setReferences(root.getReferencesOfChilds());
+      root.setReferencesOfChilds(0);
+      root.children = 0;
+      Node[] nodes = root.getNodes();
+      for (int i = 0; i < nodes.length; i++) {
+         if (root.getNode(i) != null) {
+            reduceColors(root.getNode(i));
+            root.setNode(i, null);
+         }
+      }
+   }
 
-	public Node findNodeWithLessChildsReferences(Node root) {
-		Node node = root;
-		for (int i = 0; i < root.getNodes().length; i++) {
-			if (root.getNode(i) != null) {
-				Node node2 = findNodeWithLessChildsReferences(root.getNode(i));
-				// if (node == null && ) {
-				//
-				// }
-				// else
-				if (node2.getReferencesOfChilds() <= node
-						.getReferencesOfChilds() && node2.children > 1) {
-					node = node2;
-				}
-			}
-		}
-		return node;
-	}
+   public Node findNodeWithLessChildsReferences(Node root) {
+      Node node = root;
+      for (int i = 0; i < root.getNodes().length; i++) {
+         if (root.getNode(i) != null) {
+            Node node2 = findNodeWithLessChildsReferences(root.getNode(i));
+            // if (node == null && ) {
+            //
+            // }
+            // else
+            if (node2.getReferencesOfChilds() <= node.getReferencesOfChilds() && node2.children > 1) {
+               node = node2;
+            }
+         }
+      }
+      return node;
+   }
 
-	public int getNumbersOfLeafs(Node root) {
-		int result = 0;
+   public int getNumbersOfLeafs(Node root) {
+      int result = 0;
 
-		Node[] nodes = root.getNodes();
-		boolean isLeaf = true;
-		for (int i = 0; i < nodes.length; i++) {
-			if (nodes[i] != null) {
-				isLeaf = false;
-				result += getNumbersOfLeafs(nodes[i]);
-			}
-		}
-		if (isLeaf) {
-			result = 1;
-		}
-		return result;
-	}
-	
-	public LinkedList<Color> getColorsOfLeafs(Node root, LinkedList<Color> colors) {
-		Node[] nodes = root.getNodes();
-		boolean isLeaf = true;
-		for (int i = 0; i < nodes.length; i++) {
-			if (nodes[i] != null) {
-				isLeaf = false;
-				colors = getColorsOfLeafs(nodes[i], colors);
-			}
-		}
-		if (isLeaf) {
-			colors.add(new Color((root.getRed()/root.getReferences()),(root.getGreen()/root.getReferences()),(root.getBlue()/root.getReferences())));
-		}
-		return colors;
-	}
+      Node[] nodes = root.getNodes();
+      boolean isLeaf = true;
+      for (int i = 0; i < nodes.length; i++) {
+         if (nodes[i] != null) {
+            isLeaf = false;
+            result += getNumbersOfLeafs(nodes[i]);
+         }
+      }
+      if (isLeaf) {
+         result = 1;
+      }
+      return result;
+   }
 
-	public void cluster(Node less) {
-		System.out.println("cluster");
-		Node similiar1 = null;
-		Node similiar2 = null;
-		int sim1 = 0;
-		int sim2 = 0;
-		double distance = 800.0;
-		for (int i = 0; i < less.getNodes().length; i++) {
-			Node node1 = less.getNode(i);
-			if (node1 != null) {
-				for (int j = (i + 1); j < less.getNodes().length; j++) {
-					Node node2 = less.getNode(j);
-					if (node2 != null) {
-						Double newDistance = getDistance(node1, node2);
-						System.out.println(newDistance);
-						if (newDistance != null && newDistance < distance) {
-							distance = newDistance;
-							similiar1 = node1;
-							sim1 = i;
-							similiar2 = node2;
-							sim2 = j;
-						}
-					}
-				}
-			}
-		}
-		less.getNode(sim1).setReferences(
-				less.getNode(sim1).getReferences()
-						+ less.getNode(sim2).getReferences());
-		less.getNode(sim1).setRed(
-				less.getNode(sim1).getRed() + less.getNode(sim2).getRed());
-		less.getNode(sim1).setGreen(
-				less.getNode(sim1).getGreen() + less.getNode(sim2).getGreen());
-		less.getNode(sim1).setBlue(
-				less.getNode(sim1).getBlue() + less.getNode(sim2).getBlue());
-		less.setNode(sim2, null);
-		System.out.println("sim1:" + similiar1 + "  \n sim2:" + similiar2);
-	}
+   public LinkedList<Color> getColorsOfLeafs(Node root, LinkedList<Color> colors) {
+      Node[] nodes = root.getNodes();
+      boolean isLeaf = true;
+      for (int i = 0; i < nodes.length; i++) {
+         if (nodes[i] != null) {
+            isLeaf = false;
+            colors = getColorsOfLeafs(nodes[i], colors);
+         }
+      }
+      if (isLeaf) {
+         colors.add(new Color((root.getRed() / root.getReferences()), (root.getGreen() / root.getReferences()), (root.getBlue() / root.getReferences())));
+      }
+      return colors;
+   }
 
-	private Double getDistance(Node node1, Node node2) {
-		if (node1.getReferences() != 0 && node2.getReferences() != 0) {
-			float r1 = node1.red / node1.getReferences();
-			float g1 = node1.green / node1.getReferences();
-			float b1 = node1.blue / node1.getReferences();
-			float r2 = node2.red / node2.getReferences();
-			float g2 = node2.green / node2.getReferences();
-			float b2 = node2.blue / node2.getReferences();
-			return Math.sqrt((Math.pow((r2 - r1), 2.0)
-					+ Math.pow((g2 - g1), 2.0) + Math.pow((b2 - b1), 2.0)));
-		} else {
-			return null;
-		}
-	}
+   public void cluster(Node less) {
+      System.out.println("cluster");
+      Node similiar1 = null;
+      Node similiar2 = null;
+      int sim1 = 0;
+      int sim2 = 0;
+      double distance = 800.0;
+      for (int i = 0; i < less.getNodes().length; i++) {
+         Node node1 = less.getNode(i);
+         if (node1 != null) {
+            for (int j = (i + 1); j < less.getNodes().length; j++) {
+               Node node2 = less.getNode(j);
+               if (node2 != null) {
+                  Double newDistance = getDistance(node1, node2);
+                  System.out.println(newDistance);
+                  if (newDistance != null && newDistance < distance) {
+                     distance = newDistance;
+                     similiar1 = node1;
+                     sim1 = i;
+                     similiar2 = node2;
+                     sim2 = j;
+                  }
+               }
+            }
+         }
+      }
+      less.getNode(sim1).setReferences(less.getNode(sim1).getReferences() + less.getNode(sim2).getReferences());
+      less.getNode(sim1).setRed(less.getNode(sim1).getRed() + less.getNode(sim2).getRed());
+      less.getNode(sim1).setGreen(less.getNode(sim1).getGreen() + less.getNode(sim2).getGreen());
+      less.getNode(sim1).setBlue(less.getNode(sim1).getBlue() + less.getNode(sim2).getBlue());
+      less.setNode(sim2, null);
+//      System.out.println("sim1:" + similiar1 + "  \n sim2:" + similiar2);
+   }
 
-	public void mapPictureToColors(BufferedImage resizedImage,
-			LinkedList<Color> colors) {
-		
-	}
+   private Double getDistance(Node node1, Node node2) {
+      if (node1.getReferences() != 0 && node2.getReferences() != 0) {
+         float r1 = node1.red / node1.getReferences();
+         float g1 = node1.green / node1.getReferences();
+         float b1 = node1.blue / node1.getReferences();
+         float r2 = node2.red / node2.getReferences();
+         float g2 = node2.green / node2.getReferences();
+         float b2 = node2.blue / node2.getReferences();
+         return Math.sqrt((Math.pow((r2 - r1), 2.0) + Math.pow((g2 - g1), 2.0) + Math.pow((b2 - b1), 2.0)));
+      } else {
+         return null;
+      }
+   }
 
+   private Double getDistance(Color color1, Color color2) {
+      return Math.sqrt((Math.pow((color2.getRed() - color1.getRed()), 2.0) + Math.pow((color2.getGreen() - color1.getGreen()), 2.0) + Math.pow((color2.getBlue() - color2.getBlue()), 2.0)));
+   }
+
+   public void mapPictureToColors(BufferedImage resizedImage, LinkedList<Color> colors) {
+      for (int i = 0; i < resizedImage.getHeight(); i++) {
+         for (int j = 0; j < resizedImage.getWidth(); j++) {
+            resizedImage.setRGB(j, i, getBestColor(resizedImage.getRGB(j, i), colors));
+         }
+      }
+      try {
+         ImageIO.write(resizedImage, "jpg", new File("test3.jpg"));
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+
+   private int getBestColor(int rgb, LinkedList<Color> colors) {
+      Color color = colors.get(0);
+      Color testColor = new Color(rgb);
+      double distance = getDistance(color, testColor);
+      for (int i = 1; i < colors.size(); i++) {
+         Color color1 = colors.get(i);
+         double distance1 = getDistance(color1, testColor);
+         if (distance1 < distance) {
+            color = color1;
+            distance = distance1;
+         }
+      }
+      return color.getRGB();
+   }
 }
--- Nanogramme/src/picture/PictureTestClass.java
+++ Nanogramme/src/picture/PictureTestClass.java
@@ -12,33 +12,19 @@ public class PictureTestClass {
 
    public static void main(String[] args) {
 
-      int color1 = 1;
-      int color2 = 1;
-      int color3 = 1;
-
-      int color11 = 255;
-      int color12 = 255;
-      int color13 = 255;
-
-      // int color21 = 30;
-      // int color22 = 130;
-      // int color23 = 225;
       PictureService ps = new PictureService();
       BufferedImage resizedImage = null;
       try {
          BufferedImage oi = ImageIO.read(new File("test.jpg"));
          int type = oi.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : oi.getType();
-         resizedImage = ps.scalePicture(150, 150, oi, type);
+         resizedImage = ps.scalePicture(60, 60, oi, type);
          ImageIO.write(resizedImage, "jpg", new File("test2.jpg"));
+         resizedImage = oi;
 
       } catch (IOException e) {
          e.printStackTrace();
       }
 
-      Color color = new Color(color1, color2, (color3));
-      Color colorZwei = new Color(color11, color12, color13);
-      Color colorDrei = new Color(color11, color12, (color13 - 1));
-      Color colorVier = new Color(color11, color12, (color13 - 2));
       Node root = new Node();
 
       for (int i = 0; i < resizedImage.getHeight(); i++) {
@@ -46,15 +32,15 @@ public class PictureTestClass {
             ps.insertNode(new Color(resizedImage.getRGB(j, i)), root);
          }
       }
-      System.out.println(root);
+//      System.out.println(root);
       root.copyChildSums();
       while (ps.getNumbersOfLeafs(root) > 5) {
-         System.out.println("%%%%%%%%%%");
+//         System.out.println("%%%%%%%%%%");
          Node less = ps.findNodeWithLessChildsReferences(root);
-         System.out.println(less);
-         System.out.println("&/&$§§§$%%%§$%");
+//         System.out.println(less);
+//         System.out.println("&/&$§§§$%%%§$%");
          if (less.children > (ps.getNumbersOfLeafs(root) - 4)) {
-            System.out.println("break");
+//            System.out.println("break");
             ps.cluster(less);
          } else {
             ps.reduceColors(less);
